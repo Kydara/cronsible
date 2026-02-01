128f63d4d02db057f160e4091f7e76f3b843f8f9
L1Cafe <L1Cafe@donotemail.me>
2026-02-01 21:49:25 +0100

    fix

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './templates/**/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['IBM Plex Sans', 'ui-sans-serif', 'system-ui'],
+        mono: ['Space Mono', 'ui-monospace', 'SFMono-Regular', 'monospace']
+      }
+    }
+  },
+  plugins: []
+};
